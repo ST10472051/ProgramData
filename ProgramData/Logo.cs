d15b5f0ed112528b70d4f06bf415c904939615e7
Student <Student@LabVM2047644>
2026-08-07 12:00:48 +0200

    Add project files.

---
+++ ProgramData/Logo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramData
+{
+    internal class Logo
+    {
+        public void DisplayLogo(string logo)
+        {
+            Console.WriteLine(logo);   
+            // Display a simple logo or message
+            Console.WriteLine(@"  ___           _            ___                _     
+ | __|_ _  __ _(_)_ _  ___  | _ \___ _ __  __ _(_)_ _ 
+ | _|| ' \/ _` | | ' \/ -_) |   / -_) '_ \/ _` | | '_|
+ |___|_||_\__, |_|_||_\___| |_|_\___| .__/\__,_|_|_|  
+          |___/                     |_|               ");
+        }
+    }
+}
---
+++ ProgramData/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Media;
+using ProgramData;
+using System;
+
+// Replace "Voice/greeting.wav" with the actual path to your recorded voice file
+SoundPlayer player = new SoundPlayer("C:\\Users\\Student\\source\\repos\\ProgramData\\ProgramData\\ProgramData\\voice\\output.wav");
+player.PlaySync();
+
+
+//Console.WriteLine(" ")
+ Logo logo = new Logo();
+logo.DisplayLogo("Your Logo Here");
+Console.WriteLine("Audio play successful");
+Console.ReadLine();
